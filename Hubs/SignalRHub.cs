8a9d3b8051565db3abb26d842957c0d3e599db50
Elias <98ille@gmail.com>
2021-10-11 21:09:00 +0200

    Add project files.

---
+++ Hubs/SignalRHub.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using PeerJS.Models;
+
+namespace PeerJS.Hubs
+{
+    public class SignalRHub : Hub
+    {
+
+        private Dictionary<string, ConnectionInfo> connectinInfo;
+
+        public SignalRHub(Dictionary<string, ConnectionInfo> info)
+        {
+            this.connectinInfo = info;
+        }
+
+        public async Task JoinRoom(string roomID, string peerID)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomID);
+
+            connectinInfo.Add(Context.ConnectionId, new ConnectionInfo
+            {
+                peerID = peerID,
+                roomID = roomID
+            });
+
+            await Clients.Group(roomID).SendAsync("UserConnected", peerID);
+        }
+
+        public async Task Message(string message)
+        {
+            if (connectinInfo.TryGetValue(Context.ConnectionId, out var conn))
+            {
+                await Clients.Group(conn.roomID).SendAsync("createMessage", message);
+            }
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            if (connectinInfo.TryGetValue(Context.ConnectionId, out var conn))
+            {
+                
+                    Clients.Group(conn.roomID).SendAsync("UserDisconnected", conn.peerID);
+                
+            }
+
+
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ Models/ConnectionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeerJS.Models
+{
+    public class ConnectionInfo
+    {
+        public string roomID { get; set; }
+        public string peerID { get; set; }
+    }
+}
